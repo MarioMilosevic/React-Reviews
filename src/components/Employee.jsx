2c1a0642fe18cb6a0331702e903eefc2218ce6f6
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-18 18:52:35 +0100

    Fix svg react property names

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -2,27 +2,27 @@ const Employee = ({ firstPerson }) => {
   console.log(firstPerson);
   return (
     <>
-      <article className="w-[560px] border border-black">
+      <article className="w-[560px] flex flex-col items-center justify-center border border-black">
         <img
           src={firstPerson.image}
           alt={firstPerson.image}
-          className="rounded-full"
+          className="h-[180px] w-[180px] object-cover mx-auto rounded-full"
         ></img>
-        <h2>{firstPerson.name}</h2>
-        <h3>{firstPerson.job}</h3>
+        <h2 className="capitalize">{firstPerson.name}</h2>
+        <h3 className="capitalize">{firstPerson.job}</h3>
         <p>{firstPerson.text}</p>
         <div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15.75 19.5 8.25 12l7.5-7.5"
             />
           </svg>
@@ -31,13 +31,13 @@ const Employee = ({ firstPerson }) => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="m8.25 4.5 7.5 7.5-7.5 7.5"
             />
           </svg>
